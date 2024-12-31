a7608539f7c2a626db19e7915e1d33de684e4fc7
Muhammed Favas <favas78601@gmail.com>
2024-12-31 12:05:50 +0530

    database config setup completed

---
+++ config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:1234@book-store-mern.rsuyf.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-store-MERN'
+    
